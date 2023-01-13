5c7e0fa70e5156207493d477c141c44307454abd
nikitq121 <nikitaq121@gmail.com>
2023-01-13 21:26:13 +0100

    refactored GameBoardLayout.kt, GameEndDialog.kt, GameViewModel.kt, MainActivity.kt

--- app/src/main/java/com/minesweeper/game/minesweeper/MainActivity.kt
+++ app/src/main/java/com/minesweeper/game/minesweeper/MainActivity.kt
@@ -7,21 +7,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        playB.setOnClickListener { createBoardActivity(8, 8, 10) }
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    playB.setOnClickListener { createBoardActivity(8, 8, 10) }
+  }
 
-    private fun createBoardActivity(height: Int, width: Int, amountOfMines: Int) {
-        val intent = Intent(this, GameActivity::class.java).apply {
-            GameActivity.run {
-                putExtra(INTENT_MINES, amountOfMines)
-                putExtra(INTENT_WIDTH, width)
-                putExtra(INTENT_HEIGHT, height)
-            }
-        }
-        startActivity(intent)
+  private fun createBoardActivity(height: Int, width: Int, amountOfMines: Int) {
+    val intent = Intent(this, GameActivity::class.java).apply {
+      GameActivity.run {
+        putExtra(INTENT_MINES, amountOfMines)
+        putExtra(INTENT_WIDTH, width)
+        putExtra(INTENT_HEIGHT, height)
+      }
     }
+    startActivity(intent)
+  }
 }
 
