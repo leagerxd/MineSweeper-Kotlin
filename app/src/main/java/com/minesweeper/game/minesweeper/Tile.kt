4b03cc525065d9064eae232d93a64f5dd2042c4b
nikitq121 <nikitaq121@gmail.com>
2023-01-13 21:40:03 +0100

    refactored Tile.kt, DoublyIndexedItem.kt, iterTools.kt, GameTest.kt, iterToolsTest.kt, deleted unnecessary code

--- app/src/main/java/com/minesweeper/game/minesweeper/Tile.kt
+++ app/src/main/java/com/minesweeper/game/minesweeper/Tile.kt
@@ -1,40 +1,40 @@
 package com.minesweeper.game.minesweeper
 
 open class Tile {
-    enum class State { HIDDEN, FLAGGED, MINE, NUMBERED }
+  enum class State { HIDDEN, FLAGGED, MINE, NUMBERED }
 
-    var isMine = false
-    fun plantMine() {
-        isMine = true
+  var isMine = false
+  fun plantMine() {
+    isMine = true
+  }
+
+  fun reveal() {
+    if (isRevealed) return
+    isRevealed = true
+  }
+
+  fun toggleFlag() {
+    isFlagged = !isFlagged
+  }
+
+  fun removeMine() {
+    isMine = false
+  }
+
+  fun isEmpty() = !isMine && numberOfMinedNeighbors == 0
+
+  var numberOfMinedNeighbors = 0
+  var isFlagged = false
+  var isRevealed = false
+  val state: State
+  get() = if (isRevealed) (if (isMine) State.MINE else State.NUMBERED) else (if (isFlagged) State.FLAGGED else State.HIDDEN)
+
+  override fun toString(): String {
+    return when (state) {
+      State.MINE -> "MINE"
+      State.HIDDEN -> "HIDDEN"
+      State.FLAGGED -> "FLAGGED"
+      State.NUMBERED -> numberOfMinedNeighbors.toString()
     }
-
-    fun reveal() {
-        if (isRevealed) return
-        isRevealed = true
-    }
-
-    fun toggleFlag() {
-        isFlagged = !isFlagged
-    }
-
-    fun removeMine() {
-        isMine = false
-    }
-
-    fun isEmpty() = !isMine && numberOfMinedNeighbors == 0
-
-    var numberOfMinedNeighbors = 0
-    var isFlagged = false
-    var isRevealed = false
-    val state: State
-    get() = if (isRevealed) (if (isMine) State.MINE else State.NUMBERED) else (if (isFlagged) State.FLAGGED else State.HIDDEN)
-
-    override fun toString(): String {
-        return when (state) {
-            State.MINE -> "MINE"
-            State.HIDDEN -> "HIDDEN"
-            State.FLAGGED -> "FLAGGED"
-            State.NUMBERED -> numberOfMinedNeighbors.toString()
-        }
-    }
+  }
 }
--- app/src/test/java/utils/IterToolsKtTest.kt
+++ app/src/test/java/utils/IterToolsKtTest.kt
@@ -6,8 +6,8 @@ import org.junit.Assert.*
 
 class IterToolsKtTest {
 
-    @Test
-    fun cartesianProduct() {
-        assert(listOf(1,2).cartesianProduct(listOf(1,2)) == setOf(Pair(1,1), Pair(1,2), Pair(2,2), Pair(2,1)))
-    }
+  @Test
+  fun cartesianProduct() {
+    assert(listOf(1,2).cartesianProduct(listOf(1,2)) == setOf(Pair(1,1), Pair(1,2), Pair(2,2), Pair(2,1)))
+  }
 }
